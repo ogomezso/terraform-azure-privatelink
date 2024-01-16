e2f7b8d516b53a58b35e740ef649e29b78784827
Oscar Gomez Soriano <ogomezso0@gmail.com>
2024-01-16 13:30:03 +0000

    private link + byok  full creation example

---
+++ main.tf
@@ -0,0 +1,297 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.55.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.55.0"
+    }
+  }
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+resource "confluent_environment" "env" {
+  display_name = "ogomez_azure_pl"
+}
+
+resource "confluent_schema_registry_cluster" "advanced" {
+  package = "ADVANCED"
+
+  environment {
+    id = confluent_environment.env.id
+  }
+
+  region {
+    # See https://docs.confluent.io/cloud/current/stream-governance/packages.html#stream-governance-regions
+    # Stream Governance and Kafka clusters can be in different regions as well as different cloud providers,
+    # but you should to place both in the same cloud and region to restrict the fault isolation boundary.
+    id = "sgreg-9"
+  }
+}
+
+resource "confluent_network" "private-link" {
+  display_name     = "Private Link Network"
+  cloud            = "AZURE"
+  region           = var.region
+  connection_types = ["PRIVATELINK"]
+  environment {
+    id = confluent_environment.env.id
+  }
+  dns_config {
+    resolution = "PRIVATE"
+  }
+}
+
+resource "confluent_private_link_access" "azure" {
+  display_name = "Azure Private Link Access"
+  azure {
+    subscription = var.subscription_id
+  }
+  environment {
+    id = confluent_environment.env.id
+  }
+  network {
+    id = confluent_network.private-link.id
+  }
+}
+
+# Create Confluent Cloud BYOK key
+resource "confluent_byok_key" "main" {
+  azure {
+    tenant_id      = var.tenant_id
+    key_vault_id   = azurerm_key_vault.main.id
+    key_identifier = azurerm_key_vault_key.main.versionless_id
+  }
+}
+
+resource "confluent_kafka_cluster" "dedicated" {
+  display_name = "decicated_PL"
+  availability = "SINGLE_ZONE"
+  cloud        = confluent_network.private-link.cloud
+  region       = confluent_network.private-link.region
+  dedicated {
+    cku = 2
+  }
+  environment {
+    id = confluent_environment.env.id
+  }
+  network {
+    id = confluent_network.private-link.id
+  }
+  byok_key {
+    id = confluent_byok_key.main.id
+  }
+  depends_on = [
+    azurerm_role_assignment.reader_role_assignment,
+    azurerm_role_assignment.encryption_user_role_assignment
+  ]
+}
+
+// 'app-manager' service account is required in this configuration to create 'orders' topic and assign roles
+// to 'app-producer' and 'app-consumer' service accounts.
+resource "confluent_service_account" "app-manager" {
+  display_name = "ogomez-az-app-manager"
+  description  = "Service account to manage 'inventory' Kafka cluster"
+}
+
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.dedicated.rbac_crn
+}
+
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "app-manager-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'ogomez-az-app-manager' service account"
+
+  # Set optional `disable_wait_for_ready` attribute (defaults to `false`) to `true` if the machine where Terraform is not run within a private network
+  disable_wait_for_ready = true
+
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.dedicated.id
+    api_version = confluent_kafka_cluster.dedicated.api_version
+    kind        = confluent_kafka_cluster.dedicated.kind
+
+    environment {
+      id = confluent_environment.env.id
+    }
+  }
+
+  # The goal is to ensure that
+  # 1. confluent_role_binding.app-manager-kafka-cluster-admin is created before
+  # confluent_api_key.app-manager-kafka-api-key is used to create instances of
+  # confluent_kafka_topic resource.
+  # 2. Kafka connectivity through Azure PrivateLink is setup.
+  depends_on = [
+    confluent_role_binding.app-manager-kafka-cluster-admin,
+
+    confluent_private_link_access.azure,
+    azurerm_private_dns_zone_virtual_network_link.hz,
+    azurerm_private_dns_a_record.rr,
+    azurerm_private_dns_a_record.zonal
+  ]
+}
+
+provider "azurerm" {
+  features {
+  }
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+locals {
+  hosted_zone = length(regexall(".glb", confluent_kafka_cluster.dedicated.bootstrap_endpoint)) > 0 ? replace(regex("^[^.]+-([0-9a-zA-Z]+[.].*):[0-9]+$", confluent_kafka_cluster.dedicated.rest_endpoint)[0], "glb.", "") : regex("[.]([0-9a-zA-Z]+[.].*):[0-9]+$", confluent_kafka_cluster.dedicated.bootstrap_endpoint)[0]
+  network_id  = regex("^([^.]+)[.].*", local.hosted_zone)[0]
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_subnet" "subnet" {
+  for_each = var.subnet_name_by_zone
+
+  name                 = each.value
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone" "hz" {
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  name = local.hosted_zone
+}
+
+resource "azurerm_private_endpoint" "endpoint" {
+  for_each = var.subnet_name_by_zone
+
+  name                = "confluent-${local.network_id}-${each.key}"
+  location            = var.region
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  subnet_id = data.azurerm_subnet.subnet[each.key].id
+
+  private_service_connection {
+    name                              = "confluent-${local.network_id}-${each.key}"
+    is_manual_connection              = true
+    private_connection_resource_alias = lookup(confluent_network.private-link.azure[0].private_link_service_aliases, each.key, "\n\nerror: ${each.key} subnet is missing from CCN's Private Link service aliases")
+    request_message                   = "PL"
+  }
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "hz" {
+  name                  = data.azurerm_virtual_network.vnet.name
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.hz.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_dns_a_record" "rr" {
+  name                = "*"
+  zone_name           = azurerm_private_dns_zone.hz.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  ttl                 = 60
+  records = [
+    for _, ep in azurerm_private_endpoint.endpoint : ep.private_service_connection[0].private_ip_address
+  ]
+}
+
+resource "azurerm_private_dns_a_record" "zonal" {
+  for_each = var.subnet_name_by_zone
+
+  name                = "*.az${each.key}"
+  zone_name           = azurerm_private_dns_zone.hz.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  ttl                 = 60
+  records = [
+    azurerm_private_endpoint.endpoint[each.key].private_service_connection[0].private_ip_address,
+  ]
+}
+
+#### Recursos Necesarios para el BYOK del lado AZURE
+
+data "azuread_service_principal" "current" {
+  application_id = var.client_id
+}
+
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_key_vault" "main" {
+  name                        = "ogomez-byok-keyvault"
+  location                    = data.azurerm_resource_group.rg.location
+  resource_group_name         = data.azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = var.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = true
+  enable_rbac_authorization   = true
+
+  sku_name = "standard"
+
+}
+
+resource "azurerm_role_assignment" "administrator_assignment" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azuread_service_principal.current.id
+}
+
+# Create an Azure Key
+resource "azurerm_key_vault_key" "main" {
+  name         = "ogomez-byok-key"
+  key_vault_id = azurerm_key_vault.main.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  depends_on = [azurerm_role_assignment.administrator_assignment]
+}
+
+# Create service principal referencing the application ID returned by the confluent cloud key
+resource "azuread_service_principal" "main" {
+  application_id               = confluent_byok_key.main.azure[0].application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_service_principal.current.object_id]
+}
+
+# Create role assignments to the service principal to allow Confluent access to the keyvault
+resource "azurerm_role_assignment" "reader_role_assignment" {
+  scope                = confluent_byok_key.main.azure[0].key_vault_id
+  role_definition_name = "Key Vault Reader"
+  principal_id         = azuread_service_principal.main.object_id
+}
+
+resource "azurerm_role_assignment" "encryption_user_role_assignment" {
+  scope                = confluent_byok_key.main.azure[0].key_vault_id
+  role_definition_name = "Key Vault Crypto Service Encryption User"
+  principal_id         = azuread_service_principal.main.object_id
+}
---
+++ outputs.tf
@@ -0,0 +1,13 @@
+output "resource-ids" {
+  value = <<-EOT
+  Environment ID:   ${confluent_environment.env.id}
+  Kafka Cluster ID: ${confluent_kafka_cluster.dedicated.id}
+
+  Service Accounts and their Kafka API Keys (API Keys inherit the permissions granted to the owner):
+  ${confluent_service_account.app-manager.display_name}:                     ${confluent_service_account.app-manager.id}
+  ${confluent_service_account.app-manager.display_name}'s Kafka API Key:     "${confluent_api_key.app-manager-kafka-api-key.id}"
+  ${confluent_service_account.app-manager.display_name}'s Kafka API Secret:  "${confluent_api_key.app-manager-kafka-api-key.secret}"
+
+EOT
+  sensitive = true
+}
